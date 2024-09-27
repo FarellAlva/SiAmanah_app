692e2cbaf4cd34551f93f94bb58c9003adfb9993
Alva_Force <143983961+FarellAlva@users.noreply.github.com>
2024-09-27 20:18:47 +0700

    Delete lib/src/core/services/supabase_key.dart

--- lib/src/core/services/supabase_key.dart
+++
@@ -1,7 +0,0 @@
-class Config {
-  static const supabaseUrl = 'https://bkmelxiopujbhjitnjpu.supabase.co/';
-  static const supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrbWVseGlvcHVqYmhqaXRuanB1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUzNDA2MTQsImV4cCI6MjA0MDkxNjYxNH0.uQVScMg9o2oQH8aRqgrxpb_RVYP7aJbU43Jq1LPhLoQ';
-}
-
-void alright() {}
